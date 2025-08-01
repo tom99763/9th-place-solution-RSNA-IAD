532a50ef36b4f9066715c825c67255bb6efca1cf
tom99 <jonbo99763@gmail.com>
2025-08-01 16:03:51 +0800

    update dataset downloading (location)

--- src/download_partial_files.py
+++ src/download_partial_files.py
@@ -2,26 +2,35 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 import pandas as pd
 import numpy as np
 import time
+import os
+from tqdm import tqdm
 
 dataset_name = 'rsna-intracranial-aneurysm-detection'
 
+paths = []
 def download_by_location(api, df_loc):
-    for i in range(df_loc.shape[0]):
+    for i in tqdm(range(df_loc.shape[0])):
         sample = df_loc.iloc[i]
         SeriesInstanceUID = sample['SeriesInstanceUID']
         SOPInstanceUID = sample['SOPInstanceUID']
         path = f'series/{SeriesInstanceUID}/{SOPInstanceUID}.dcm'
-        api.competition_download_file(dataset_name, path, path='./rsna_data/loc_slices')
-        time.sleep(5)
+        try:
+            api.competition_download_file(dataset_name, path, path='./rsna_data/loc_slices')
+            paths.append(path)
+        except:
+            pass
 
 
 def download_data():
     api = KaggleApi()
     api.authenticate()
-    api.competition_download_file(
+    try:
+        api.competition_download_file(
         dataset_name, 'train.csv', path='./rsna_data')
-    api.competition_download_file(dataset_name, 'train_localizers.csv',
+        api.competition_download_file(dataset_name, 'train_localizers.csv',
                                   path='./rsna_data')
+    except:
+        pass
     df_meta = pd.read_csv('./rsna_data/train.csv')
     df_loc = pd.read_csv('./rsna_data/train_localizers.csv')
     download_by_location(api, df_loc)
