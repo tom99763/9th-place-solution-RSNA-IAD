f1f97b57dd087201d271017886d14fd5a3dc88b4
tom99 <jonbo99763@gmail.com>
2025-08-06 23:38:11 +0800

    update fine-tune

--- src/vesselfm/module.py
+++ src/vesselfm/module.py
@@ -44,6 +44,9 @@ class RSNAModuleFinetune(lightning.LightningModule):
 
     def validation_step(self, batch, batch_idx):
         image, mask = batch
+        print(image.device)
+        print(image.shape, mask.shape)
+        mask = mask.float()
         with torch.no_grad():
             pred_mask = self.sliding_window_inferer(image, self.model)
             loss = self.loss(pred_mask, mask)
