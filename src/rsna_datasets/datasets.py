depth slice with fixed normalization

src/rsna_datasets/datasets.py
@@ -39,6 +39,7 @@ class NpzVolumeSliceDataset(Dataset):
         uid = self.uids[idx]
         rowdf = self.train_df[self.train_df["SeriesInstanceUID"] == uid]
         labeldf = self.label_df[self.label_df["SeriesInstanceUID"] == uid]
+
         with np.load(self.data_path / "slices" / f"{uid}.npz") as data:
             volume = data['vol'].astype(np.float32)
 
@@ -52,31 +53,20 @@ class NpzVolumeSliceDataset(Dataset):
             if int(rowdf["Aneurysm Present"].iloc[0]) == 1:
                 slice_idx = random.choice(labeldf["z"].tolist())
                 class_idxs = [LABELS_TO_IDX[c] for c in  labeldf[labeldf["z"] == slice_idx]["location"]]
-                slice = volume[slice_idx]
 
                 loc_labels[class_idxs] = 1
                 label = 1
 
             else:
-                
-                # items = list(range(volume.shape[0]))
-                # mu = volume.shape[0] / 2
-                # sigma = volume.shape[0] * 0.25
-                #
-                #
-                # weights = np.exp(-((items - mu) ** 2) / (2 * sigma ** 2))
-                # weights /= weights.sum()
-                #
-                # # Sample one item
-                # sample = np.random.choice(items, p=weights)
-                # slice = volume[sample, :, :]
-
                 slice_idx = np.random.randint(0, volume.shape[0])
-                slice = volume[slice_idx, :, :]
 
-          
-            img = np.stack([slice] * 3, axis=-1)
-             
+
+            
+            img = np.stack([ volume[max(slice_idx - 1,0)] # slice_idx - 1
+                           , volume[slice_idx] # slice_idx
+                           , volume[min(slice_idx + 1, volume.shape[0] - 1)] # slice_idx + 1
+                           ], axis=-1)
+
          
             # Apply transforms if any (e.g., normalization, resizing)
             if self.transform:
@@ -88,7 +78,7 @@ class NpzVolumeSliceDataset(Dataset):
             loc_labels = np.zeros((volume.shape[0],self.num_classes))
             label = np.zeros(volume.shape[0])
 
-            volume = np.stack([volume,volume,volume],axis=1)
+            volume = self.create_rgb_slices(volume)
 
             if self.transform:
                 volume = self.transform(image=volume)["image"]
@@ -100,6 +90,17 @@ class NpzVolumeSliceDataset(Dataset):
                     label[slice_idx] = 1
 
             return volume, label, loc_labels
+
+    def create_rgb_slices(self, volume):
+
+        D, H, W = volume.shape
+        rgb_slices = []
+
+        for i in range(0, D):
+            rgb = np.stack([volume[max(0, i - 1)], volume[i], volume[min(i + 1, D - 1)]], axis=0)
+            rgb_slices.append(rgb)
+
+        return np.stack(rgb_slices, axis=0)
             
 
 class NpzDataModule(pl.LightningDataModule):
@@ -109,7 +110,7 @@ class NpzDataModule(pl.LightningDataModule):
         self.cfg = cfg
 
         self.train_transforms = A.Compose([
-            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            A.Normalize(mean=(0.485, 0.485, 0.485), std=(0.229, 0.229, 0.229)),
             A.HorizontalFlip(p=0.5),
             A.ShiftScaleRotate( shift_limit=0.06, scale_limit=0.1, rotate_limit=15, p=0.7),
             A.ElasticTransform(p=0.3, alpha=10, sigma=120 * 0.05, alpha_affine=120 * 0.03),
@@ -117,7 +118,7 @@ class NpzDataModule(pl.LightningDataModule):
             ToTensorV2(), # This should be the last step if needed
         ])
         self.val_transforms = A.Compose([
-            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            A.Normalize(mean=(0.485, 0.485, 0.485), std=(0.229, 0.229, 0.229)),
         ])
 
     def setup(self, stage: str = None):

validation.py
@@ -1,4 +1,3 @@
-
 import hydra
 from omegaconf import DictConfig, OmegaConf
 from sklearn.metrics import roc_auc_score
@@ -7,31 +6,40 @@ import numpy as np
 import pandas as pd
 import pytorch_lightning as pl
 from pathlib import Path
-from configs import data_config
 from hydra.utils import instantiate
 from tqdm import tqdm
 
-import torch.cuda.amp as amp
-
 torch.set_float32_matmul_precision('medium')
 
-val_transforms = A.Compose([ A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)) ])
+mean = np.array([0.485, 0.485, 0.485], dtype=np.float32).reshape((1,3,1,1))
+std = np.array([0.229, 0.229, 0.229], dtype=np.float32).reshape((1,3,1,1))
 
+def create_rgb_slices(volume):
+
+    D, H, W = volume.shape
+    rgb_slices = []
+
+    for i in range(0, D):
+        rgb = np.stack([volume[max(0, i - 1)], volume[i], volume[min(i + 1, D - 1)]], axis=0)
+        rgb_slices.append(rgb)
+
+    return np.stack(rgb_slices, axis=0)
 
 @torch.no_grad()
 def eval_one_series(volume, model):
 
-    volume = np.stack([volume,volume,volume],axis=1)
-    volume = val_transforms(image=volume)["image"]
+    volume = create_rgb_slices(volume)
+    volume = (volume / 255 - mean) / std
     volume = torch.from_numpy(volume).cuda()
   
     pred_cls = []
     pred_locs = []
 
-    for batch_idx in range(0,volume.shape[0], 64):
-        pc,pl = model(volume[batch_idx:batch_idx+64])
-        pred_cls.append(pc)
-        pred_locs.append(pl)
+    with torch.cuda.amp.autocast():
+        for batch_idx in range(0,volume.shape[0], 64):
+            pc,pl = model(volume[batch_idx:batch_idx+64])
+            pred_cls.append(pc)
+            pred_locs.append(pl)
 
     pred_cls = torch.vstack(pred_cls)
     pred_locs = torch.vstack(pred_locs)
@@ -62,7 +70,7 @@ def validation(cfg: DictConfig) -> None:
     pl.seed_everything(cfg.seed)
 
     model = instantiate(cfg.model, pretrained=False)
-    model_ckpt_name="efficient_b2_gaussian_sample-epoch=47-val_loss=0.4765_fold_id=0"
+    model_ckpt_name="efficient_b2_depth_slices-epoch=41-val_loss=0.6973_fold_id=0"
     # pl_model = LitTimmClassifier.load_from_checkpoint(f"./models/{model_ckpt_name}.ckpt", model=model)
     # torch.save(pl_model.model.state_dict(), f"{model_ckpt_name}.pth")
     # return
@@ -92,7 +100,7 @@ def validation(cfg: DictConfig) -> None:
         cls_labels.append(rowdf["Aneurysm Present"].iloc[0])
         loc_labels.append(loc_label)
 
-        with np.load(f"./data/processed/{uid}.npz") as data:
+        with np.load(f"./data/processed/slices/{uid}.npz") as data:
             volume = data['vol'].astype(np.float32)
             cls_prob, loc_probs =  eval_one_series(volume, model)
 