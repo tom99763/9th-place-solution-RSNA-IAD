a17603a484878961fb08c5e108b78ff8d7522401
tom99 <jonbo99763@gmail.com>
2025-08-14 10:02:38 +0800

    update gnn training

--- src/trainers/gnn_trainer.py
+++ src/trainers/gnn_trainer.py
@@ -27,25 +27,26 @@ class GNNClassifier(pl.LightningModule):
         self.automatic_optimization = False
 
     def training_step(self, data, _):
-        cls_labels, loc_labels = data.cls_labels, data.loc_labels
+        cls_labels = data.cls_labels
+        loc_labels = data.loc_labels
 
         pred_cls, pred_locs = self.model(data)
-        pred_cls = pred_cls.squeeze()
 
+        # Compute losses
         loc_loss = self.loc_loss_fn(pred_locs, loc_labels)
         cls_loss = self.cls_loss_fn(pred_cls, cls_labels)
 
-        loss = (cls_loss + loc_loss) / 2
+        loss = 0.5 * (cls_loss + loc_loss)
 
+        # Update metrics
         self.train_loc_auroc.update(pred_locs, loc_labels.long())
         self.train_cls_auroc.update(pred_cls, cls_labels.long())
 
-        self.log('train_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
-        # Log the metric object. Lightning computes and logs it at epoch end.
+        self.log('train_loss', loss, on_step=False, on_epoch=True, prog_bar=True)
         self.log('train_loc_auroc', self.train_loc_auroc, on_step=False, on_epoch=True, prog_bar=True)
         self.log('train_cls_auroc', self.train_cls_auroc, on_step=False, on_epoch=True, prog_bar=True)
 
-        # Manual backward pass
+        # Manual optimization
         opt = self.optimizers()
         opt.zero_grad()
         self.manual_backward(loss)
@@ -54,22 +55,24 @@ class GNNClassifier(pl.LightningModule):
         return loss
 
     def validation_step(self, data, batch_idx):
-        cls_labels, loc_labels = data.cls_labels, data.loc_label
+        cls_labels = data.cls_labels
+        loc_labels = data.loc_labels
+
         with torch.no_grad():
             pred_cls, pred_locs = self.model(data)
 
         loc_loss = self.loc_loss_fn(pred_locs, loc_labels)
         cls_loss = self.cls_loss_fn(pred_cls, cls_labels)
 
-        loss = (cls_loss + loc_loss) / 2
+        loss = 0.5 * (cls_loss + loc_loss)
 
         self.val_loc_auroc.update(pred_locs, loc_labels.long())
         self.val_cls_auroc.update(pred_cls, cls_labels.long())
 
-        self.log('val_loss', loss, on_step=False, on_epoch=True, logger=True, prog_bar=True)
-        # Log the metric object. Lightning computes and logs it at epoch end.
+        self.log('val_loss', loss, on_step=False, on_epoch=True, prog_bar=True)
         self.log('val_loc_auroc', self.val_loc_auroc, on_step=False, on_epoch=True, prog_bar=True)
         self.log('val_cls_auroc', self.val_cls_auroc, on_step=False, on_epoch=True, prog_bar=True)
+
         return loss
 
     def on_train_epoch_end(self):
@@ -79,6 +82,14 @@ class GNNClassifier(pl.LightningModule):
         if isinstance(sch, torch.optim.lr_scheduler.ReduceLROnPlateau) and (
                 self.current_epoch + 1) % self.cfg.trainer.check_val_every_n_epoch == 0:
             sch.step(self.trainer.callback_metrics["val_loss"])
+
+    def on_train_epoch_start(self):
+        self.train_loc_auroc.reset()
+        self.train_cls_auroc.reset()
+
+    def on_validation_epoch_start(self):
+        self.val_loc_auroc.reset()
+        self.val_cls_auroc.reset()
 
     def configure_optimizers(self):
         optimizer = instantiate(self.cfg.optimizer, params=self.parameters())
