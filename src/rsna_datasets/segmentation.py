12d25a804c08283e9de818ce1ac26b62fedfd3d3
IAmParadox <confusedstudent13@gmail.com>
2025-08-25 23:00:53 +0530

    complete dataset

--- src/rsna_datasets/segmentation.py
+++ src/rsna_datasets/segmentation.py
@@ -10,21 +10,22 @@ import albumentations as A
 from pathlib import Path
 from configs.data_config import *
 from pathlib import Path
+import cv2
 
 from monai.transforms import MapTransform, RandCropByLabelClassesd
 
 torch.set_float32_matmul_precision('medium')
 
 class DynamicRandCropByLabelClassesd(MapTransform):
-    def __init__(self, keys, label_key="label", spatial_size=(4,128,128), num_classes=14, num_samples=1):
+    def __init__(self, keys, label_key="label", num_classes=14, num_samples=1):
         super().__init__(keys)
         self.label_key = label_key
         self.num_classes = num_classes
-        self.spatial_size = spatial_size
         self.num_samples = num_samples
 
     def __call__(self, data):
         d = dict(data)
+        spatial_size = d.pop("spatial_size")
 
         # get unique labels from this mask
         labels = d[self.label_key]
@@ -43,12 +44,13 @@ class DynamicRandCropByLabelClassesd(MapTransform):
         cropper = RandCropByLabelClassesd(
             keys=self.keys,
             label_key=self.label_key,
-            spatial_size=self.spatial_size,
+            spatial_size=spatial_size,
             num_classes=self.num_classes,
             num_samples=self.num_samples,
             ratios=ratios,
         )
-        return cropper(d)
+        cropped = cropper(d)
+        return cropped[0], cropper.cropper.centers[0]
 
 
 
@@ -56,7 +58,7 @@ class WindowedMIP(Dataset):
     def __init__(self, uids, cfg, transform=None, mode="train"):
         self.uids = uids
         self.cfg = cfg
-        self.data_path = Path(cfg.param.data_path)
+        self.data_path = Path(cfg.params.data_path)
         self.traindf = pd.read_csv(self.data_path / "train.csv")
         self.extract_patch = DynamicRandCropByLabelClassesd(
                     keys=["image", "label"],
@@ -64,11 +66,70 @@ class WindowedMIP(Dataset):
                     num_classes=self.cfg.params.num_classes,   # required since label is one-hot
                     num_samples=1,
         )
+        self.transform = transform
+        self.mode = mode
 
     def __len__(self):
         return len(self.uids)
 
     def __getitem__(self, idx):
+        if self.mode == "train":
+            return self.get_train_sample(idx)
+        else:
+            return self.get_val_sample(idx)
+
+    def get_val_sample(self, idx):
+
+
+        uid = self.uids[idx]
+        series_info = self.traindf[self.traindf["SeriesInstanceUID"] == uid]
+        modality = series_info["Modality"].iloc[0]
+
+        sample = np.load(self.data_path / f"{uid}.npz")
+
+        vol,coords,loc_labels = sample["vol"],sample["coords"], sample["location_labels"]
+        mask = np.zeros_like(vol, dtype=np.uint8)
+
+        vol_depth,_,_ = vol.shape
+
+        for coord,label in zip(coords,loc_labels):
+            mask = self.create_hard_ball_mask(mask,coord,label)
+
+        if modality == "CTA":
+            z_stride = self.cfg.params.sliding_stride.cta
+            spatial_size = np.array(self.cfg.params.spatial_size.cta)
+        else:
+            z_stride = self.cfg.params.sliding_stride.mr
+            spatial_size = np.array(self.cfg.params.spatial_size.mr)
+
+
+        images = []
+        masks = []
+
+        (patch_z,_,_) = spatial_size
+
+        for center_z in range(0, vol_depth, z_stride):
+            slice     = vol[center_z]
+            sidx = max(0, center_z-patch_z//2)
+            eidx = center_z+patch_z//2+1
+            slice_mip = vol[sidx:eidx].max(axis=0)
+
+            images.append(np.stack([slice_mip,slice], axis=0))
+            masks.append(mask[sidx:eidx].max(axis=0))
+
+        images = np.stack(images, axis=0)
+        masks  = np.stack(masks, axis=0)
+
+        if self.transform:
+            transformed = self.transform(image=images, label=masks)
+            images = transformed["image"]
+            masks = transformed["label"]
+
+        return images, masks
+
+    def get_train_sample(self, idx):
+
+
         uid = self.uids[idx]
         series_info = self.traindf[self.traindf["SeriesInstanceUID"] == uid]
         modality = series_info["Modality"].iloc[0]
@@ -79,7 +140,7 @@ class WindowedMIP(Dataset):
         mask = np.zeros_like(vol, dtype=np.uint8)
 
         for coord,label in zip(coords,loc_labels):
-            mask = self.create_hard_ball_mask(mask,coord,label + 1)
+            mask = self.create_hard_ball_mask(mask,coord,label)
 
         if modality == "CTA":
 
@@ -88,45 +149,31 @@ class WindowedMIP(Dataset):
         else:
             spatial_size = np.array(self.cfg.params.spatial_size.mr)
 
-        patch = self.extract_patch({"image": np.expand_dims(vol, 0), "label": np.expand_dims(mask, 0), "spatial_size": spatial_size})[0]
+        (patch_z,_,_) = spatial_size
+        patch, (center_z,_,_) = self.extract_patch({"image": np.expand_dims(vol, 0), "label": np.expand_dims(mask, 0), "spatial_size": spatial_size})
 
-        # 4x128x128
+        img_width,img_height = (self.cfg.params.img_width, self.cfg.params.img_height)
+
+        # ZxYxX
         patch_image = patch["image"][0].numpy()
+        print(f"{patch_image.shape=}")
+        patch_slice = cv2.resize(patch_image[patch_z//2], (img_width, img_height))
+        patch_mip   = cv2.resize(patch_image.max(axis=0), (img_width, img_height))
 
-        # 4x128x128
-        patch_mask  = patch["label"][0].numpy()
+        slice     = cv2.resize(vol[center_z], (img_width, img_height))
+        slice_mip = cv2.resize(vol[center_z-patch_z//2:center_z+patch_z//2+1].max(axis=0), (img_width, img_height))
 
-        # clsx4x128x128 -> clsx128x128
-        mask = self.one_hot_encode_mask(patch_mask).max(axis=1)
-        patch_label = patch_mask[*(spatial_size // 2)]
+        patch_mask  = cv2.resize(patch["label"][0].numpy().T, (img_width, img_height)).T
+        mask = patch_mask.max(axis=0)
 
-        if (patch_label != 0):
-            cidx = np.where(loc_labels == patch_label)[0][0]
-            z1 = coords[cidx]
-            slice = vol[z1]
-            mip_slice = vol[z1]
+        image = np.stack([patch_mip,patch_slice, slice_mip, slice])
 
-        return 
+        if self.transform:
+            transformed = self.transform(image=image, label=mask)
+            image = transformed["image"]
+            mask = transformed["label"]
 
-
-
-
-    def one_hot_encode_mask(self, mask):
-        """
-        Convert mask of shape (D, H, W) with labels in [0, num_classes]
-        into one-hot encoding of shape (num_classes, D, H, W).
-        """
-
-        # create one-hot using broadcasting
-        onehot = np.eye(self.cfg.params.num_classes, dtype=np.uint8)[mask]   # (D,H,W,num_classes)
-
-        # move channels to front -> (num_classes, D, H, W)
-        onehot = np.moveaxis(onehot, -1, 0)
-
-        return onehot
-
-
-
+        return image, mask
 
     def create_hard_ball_mask(self, mask, center, fill_value):
         volume_shape = mask.shape
