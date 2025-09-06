7a30d79cd741fdeb998598a35973bc045748ac94
tom99 <jonbo99763@gmail.com>
2025-09-06 10:45:33 +0800

    update

--- src/model.py
+++ src/model.py
@@ -15,7 +15,7 @@ class GraphModel(nn.Module):
                              out_channels=1,
                              jk=jk,
                              dropout=dropout,
-                             norm=LayerNorm(hidden_channels, affine=True, mode = "node")
+                             norm=LayerNorm(hidden_channels)
                              )
 
     def forward(self, data):
