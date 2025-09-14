5941bc0ed1704ba7f09adcf23d2f7bbca9d0aa0c
tom99 <jonbo99763@gmail.com>
2025-09-14 23:45:44 +0800

    update

--- src/vesselfm/dataset.py
+++ src/vesselfm/dataset.py
@@ -70,7 +70,7 @@ def _generate_transforms(vol_size, input_size, mode):
                 image_key="Image",
                 allow_smaller=True,
             ),
-            ModalityIntensityScalingd(keys=["Image"]),
+            #ModalityIntensityScalingd(keys=["Image"]),
             SpatialPadd(keys=["Image", "Mask"], spatial_size=input_size, mode="constant", method="symmetric"),
             ToTensord(keys=["Image", "Mask"]),
         ])
@@ -79,7 +79,7 @@ def _generate_transforms(vol_size, input_size, mode):
             EnsureChannelFirstd(keys=["Image", "Mask"], channel_dim="no_channel"),
             EnsureTyped(keys=["Image", "Mask"]),
             Resized(keys=["Image", "Mask"], spatial_size=vol_size, mode=["trilinear", "nearest"]),
-            ModalityIntensityScalingd(keys=["Image"]),
+            #ModalityIntensityScalingd(keys=["Image"]),
             ToTensord(keys=["Image", "Mask"]),
         ])
 
@@ -92,7 +92,7 @@ class RSNASegDataset(Dataset):
         print(dataset_config)
         self.data_path = dataset_config.path
         self.uids = uids
-        self.reader = determine_reader_writer(dataset_config.file_format)()
+        #self.reader = determine_reader_writer(dataset_config.file_format)()
         self.transforms = _generate_transforms(
             dataset_config['vol_size'], dataset_config['input_size'], mode) #generate_transforms(dataset_config.transforms[mode])
         self.mode = mode
@@ -102,10 +102,21 @@ class RSNASegDataset(Dataset):
 
     def __getitem__(self, idx: int):
         uid = self.uids[idx]
-        vol_path = f'{self.data_path}/segmentations/{uid}.nii'
+        vol_path = f'{self.data_path}/seg_vols/{uid}.npz'
         mask_path = f'{self.data_path}/segmentations/{uid}_cowseg.nii'
-        vol = self.reader.read_images(vol_path)[0].astype(np.float32)
-        mask = self.reader.read_images(mask_path)[0]
+        # vol = self.reader.read_images(vol_path)[0].astype(np.float32)
+        # mask = self.reader.read_images(mask_path)[0]
+
+        #mask
+        mask_data = nib.load(mask_path)
+        mask = mask_data.get_fdata()
+        affine = mask_data.affine
+        mask = np.flip(mask, axis=0)
+        mask = mask.transpose(2, 1, 0)
+        mask = np.flip(mask, axis=1) #z, y, x
+
+        #volume
+        vol_data = np.load(vol_path)['vol'][..., 0] #z, y, x
         transformed = self.transforms({'Image': vol, 'Mask': mask})
         if self.mode == 'train':
             return transformed
