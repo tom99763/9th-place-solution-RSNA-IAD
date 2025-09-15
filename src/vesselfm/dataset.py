0cf321d56a90ca8917020bfa7bbc0373092cd900
tom99 <jonbo99763@gmail.com>
2025-09-16 07:12:29 +0800

    update

--- src/vesselfm/dataset.py
+++ src/vesselfm/dataset.py
@@ -67,7 +67,7 @@ def _generate_transforms(vol_size, input_size, mode):
                 spatial_size=input_size,
                 num_classes=14,
                 ratios = [0] + [1] * 13,
-                num_samples=8,
+                num_samples=2,
                 image_key="Image",
                 allow_smaller=True,
             ),
@@ -93,7 +93,7 @@ class RSNASegDataset(Dataset):
         print(dataset_config)
         self.data_path = dataset_config.path
         self.uids = uids
-        self.reader = determine_reader_writer(dataset_config.file_format)()
+        #self.reader = determine_reader_writer(dataset_config.file_format)()
         self.transforms = _generate_transforms(
             dataset_config['vol_size'], dataset_config['input_size'], mode) #generate_transforms(dataset_config.transforms[mode])
         self.mode = mode
@@ -103,20 +103,18 @@ class RSNASegDataset(Dataset):
 
     def __getitem__(self, idx: int):
         uid = self.uids[idx]
-        vol_path = f'{self.data_path}/segmentations/{uid}.nii'
+        vol_path = f'{self.data_path}/seg_vols/{uid}.npz'
         mask_path = f'{self.data_path}/segmentations/{uid}_cowseg.nii'
-        vol = self.reader.read_images(vol_path)[0].astype(np.float32)
-        mask = self.reader.read_images(mask_path)[0]
 
-        # #mask
-        # mask_data = nib.load(mask_path)
-        # mask = mask_data.get_fdata()
-        # mask = np.flip(mask, axis=0).copy()
-        # mask = mask.transpose(2, 1, 0).copy()
-        # mask = np.flip(mask, axis=1).copy()
+        #mask
+        mask_data = nib.load(mask_path)
+        mask = mask_data.get_fdata()
+        mask = np.flip(mask, axis=0).copy()
+        mask = mask.transpose(2, 1, 0).copy()
+        mask = np.flip(mask, axis=1).copy()
 
         #volume
-        # vol = np.load(vol_path, mmap_mode='r')['vol'].copy()[..., 0]
+        vol = np.load(vol_path, mmap_mode='r')['vol'].copy()[..., 0]
         transformed = self.transforms({'Image': vol, 'Mask': mask})
         if self.mode == 'train':
             return transformed
