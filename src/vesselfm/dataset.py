8dde279de0e0c621315ddb5c2bc0963455379ca0
tom99 <jonbo99763@gmail.com>
2025-09-16 00:40:21 +0800

    update

--- src/vesselfm/dataset.py
+++ src/vesselfm/dataset.py
@@ -61,25 +61,25 @@ def _generate_transforms(vol_size, input_size, mode):
             EnsureChannelFirstd(keys=["Image", "Mask"], channel_dim="no_channel"),
             EnsureTyped(keys=["Image", "Mask"]),
             Resized(keys=["Image", "Mask"], spatial_size=vol_size, mode=["trilinear", "nearest"]),
-            RandCropByLabelClassesd(
-                keys=["Image", "Mask"],
-                label_key="Mask",
-                spatial_size=input_size,
-                num_classes=14,
-                ratios = [0] + [1] * 13,
-                num_samples=4,
-                image_key="Image",
-                allow_smaller=True,
-            ),
-            # RandCropByPosNegLabeld(
+            # RandCropByLabelClassesd(
             #     keys=["Image", "Mask"],
             #     label_key="Mask",
             #     spatial_size=input_size,
-            #     pos=1,
-            #     neg=0,
-            #     num_samples=4,
-            #     allow_smaller=True
+            #     num_classes=13,
+            #     ratios = [1] * 13,
+            #     num_samples=16,
+            #     image_key="Image",
+            #     allow_smaller=True,
             # ),
+            RandCropByPosNegLabeld(
+                keys=["Image", "Mask"],
+                label_key="Mask",
+                spatial_size=input_size,
+                pos=1,
+                neg=0,
+                num_samples=4,
+                allow_smaller=True
+            ),
             ModalityIntensityScalingd(keys=["Image"]),
             SpatialPadd(keys=["Image", "Mask"], spatial_size=input_size, mode="constant", method="symmetric"),
             ToTensord(keys=["Image", "Mask"]),
@@ -102,7 +102,7 @@ class RSNASegDataset(Dataset):
         print(dataset_config)
         self.data_path = dataset_config.path
         self.uids = uids
-        self.reader = determine_reader_writer(dataset_config.file_format)()
+        #self.reader = determine_reader_writer(dataset_config.file_format)()
         self.transforms = _generate_transforms(
             dataset_config['vol_size'], dataset_config['input_size'], mode) #generate_transforms(dataset_config.transforms[mode])
         self.mode = mode
@@ -112,20 +112,20 @@ class RSNASegDataset(Dataset):
 
     def __getitem__(self, idx: int):
         uid = self.uids[idx]
-        vol_path = f'{self.data_path}/segmentations/{uid}.nii'
+        vol_path = f'{self.data_path}/seg_vols/{uid}.npz'
         mask_path = f'{self.data_path}/segmentations/{uid}_cowseg.nii'
-        vol = self.reader.read_images(vol_path)[0].astype(np.float32)
-        mask = self.reader.read_images(mask_path)[0]
+        # vol = self.reader.read_images(vol_path)[0].astype(np.float32)
+        # mask = self.reader.read_images(mask_path)[0]
 
-        # #mask
-        # mask_data = nib.load(mask_path)
-        # mask = mask_data.get_fdata()
-        # mask = np.flip(mask, axis=0).copy()
-        # mask = mask.transpose(2, 1, 0).copy()
-        # mask = np.flip(mask, axis=1).copy()
+        #mask
+        mask_data = nib.load(mask_path)
+        mask = mask_data.get_fdata()
+        mask = np.flip(mask, axis=0).copy()
+        mask = mask.transpose(2, 1, 0).copy()
+        mask = np.flip(mask, axis=1).copy()
 
         #volume
-        # vol = np.load(vol_path, mmap_mode='r')['vol'].copy()
+        vol = np.load(vol_path, mmap_mode='r')['vol'].copy()
         transformed = self.transforms({'Image': vol, 'Mask': mask})
         if self.mode == 'train':
             return transformed
