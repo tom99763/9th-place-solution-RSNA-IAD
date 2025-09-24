8b963956b88c596c61aef0722627b9ff28dd112b
tom99 <jonbo99763@gmail.com>
2025-09-24 20:29:28 +0800

    update

---
+++ src/clean_empty_patche_files.py
@@ -0,0 +1,12 @@
+import os
+
+data_path = './data/patch_data'
+
+for i in range(2):
+    dir_path = os.listdir(f'{data_path}/fold{i}')
+    count_zero = 0
+    for uid in dir_path:
+        if len(os.listdir(f'{data_path}/fold{i}/{uid}'))==0:
+            count_zero+=1
+            os.rmdir(f'{data_path}/fold{i}/{uid}')
+    print(count_zero)
